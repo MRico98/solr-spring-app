d3869e77b8540c7ecb502e3c701043ac57566383
Rico <ManuelRico98@outlook.es>
2021-02-01 21:02:58 -0600

    modificaciones de botones

--- src/main/java/com/solr/demo/view/js/resultPageScript.js
+++ src/main/java/com/solr/demo/view/js/resultPageScript.js
@@ -1,10 +1,48 @@
+const urlSearch = "http://localhost:8080/api/search";
+const urlSpellChecker = "http://localhost:8080/api/spellchecker"
+const urlSuggestion = "http://localhost:8080/api/suggestion";
+
 $(document).ready(function(){
     setActualQuery(JSON.parse(sessionStorage.getItem('jsonresponse')));
     setResults(JSON.parse(sessionStorage.getItem('jsonresponse')));
     setFacetSearch(JSON.parse(sessionStorage.getItem('jsonresponse')).facet_counts.facet_fields.host);
     setCorrection(JSON.parse(sessionStorage.getItem('jsonspellchecker')).spellcheck);
     setSuggester(JSON.parse((sessionStorage.getItem('jsonsuggestion'))).suggest.mySuggester);
+    $("#searching + input").click(function() {
+        var barraBusqueda = $("#searching");
+        executeQuery(barraBusqueda.val(),urlSearch,'jsonresponse');
+        executeQuery(barraBusqueda.val(),urlSpellChecker,'jsonspellchecker');
+        executeQuery(barraBusqueda.val(),urlSuggestion,'jsonsuggestion');
+        window.location = "resultspage.html";
+    });
 });
+
+async function executeQuery(query,url,storageVariable){
+    $.ajax({
+        url: setUrl(query,url),
+        type: "GET",
+        crossDomain: true,
+        dataType: 'json',
+        beforeSend: function(request) {
+            request.setRequestHeader("Access-Control-Allow-Origin", "localhost");
+        },
+        async: false
+    }).done(function (data, textStatus, jqXHR) {
+        console.log(JSON.stringify(data));
+        sessionStorage.setItem(storageVariable,JSON.stringify(data));
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+        console.log(jqXHR);
+        console.log(textStatus);
+    });
+}
+
+function setUrl(query,urlQuery){
+    if(query){
+        urlQuery += "?query=" + query;
+    }
+    console.log(urlQuery);
+    return urlQuery;
+}
 
 function setActualQuery(response){
     $('#searchaside input:first-child').val(response.responseHeader.params.q)
